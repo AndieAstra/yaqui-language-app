9830ccfb4d479dbb72c81dbe6f46327d8a463110
Andres Astra <andieroid@arizona.edu>
2024-12-24 15:41:26 -0500

    now working on course page

--- a-aniame/app/main/learn/page.tsx
+++ a-aniame/app/main/learn/page.tsx
@@ -1,9 +1,32 @@
+import { StickyWrapper } from "@/components/sticky-wrapper"
+import { FeedWrapper } from "@/components/feed-wrapper"
+import { UserProgress } from "@/components/user-progress"
+
+import { Header } from "./header"
+
 export default function Home() {
 
    return(
    
-   <div>
-     Learn Page
+   <div className="flex flex-row-reverse gap-[48px] px-6">
+
+    <StickyWrapper>
+      <UserProgress
+      activeCourse={{ title: "Spanish", imageSrc: "/mx.svg" }}
+      hearts={5}
+      points={100}
+      hasActiveSubscription={false}
+      />
+      </StickyWrapper>
+
+      <FeedWrapper>
+      <Header title="Spanish"/>
+         <div className="space-y-4">
+
+
+         </div>
+      </FeedWrapper>
+
    </div>
    
    )
